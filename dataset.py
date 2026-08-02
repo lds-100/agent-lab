8005557cea930bb75b696cbd174fc46011f5c73b
lindaschilling <linda.d.schilling@gmail.com>
2026-08-02 19:13:42 +0200

    Add reward to saved trajectories

--- dataset.py
+++ dataset.py
@@ -1,6 +1,8 @@
 import json
 
 
-def save_trajectory(trajectory, path="trajectories.jsonl"):
+def save_trajectory(trajectory, reward, path="trajectories.jsonl"):
+    trajectory["reward"] = reward
+
     with open(path, "a") as f:
         f.write(json.dumps(trajectory) + "\n")
