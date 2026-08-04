35939dc19bd9e42dff1603697d2bdc9d87825981
lindaschilling <linda.d.schilling@gmail.com>
2026-08-04 12:01:49 +0200

    Add LoRA to model due to running out of GPU memory

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch
+from peft import LoraConfig, get_peft_model
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_name = "Qwen/Qwen2.5-1.5B-Instruct"
@@ -10,3 +11,24 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.float16,
     device_map="auto",
 )
+
+lora_config = LoraConfig(
+    r=8,
+    lora_alpha=16,
+    lora_dropout=0.05,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+    ],
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(
+    model,
+    lora_config,
+)
+
+model.print_trainable_parameters()
